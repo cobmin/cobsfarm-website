8d3fcadb0758d52b56b2a3845a6256649f7cd3f2
cobmin <jacob-huber@outlook.com>
2023-09-22 09:05:46 -0500

    fixing &apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Cob's Farm - Adventure Game with Farming and Combat</title>
+        <title>Cob&apos;s Farm - Adventure Game with Farming and Combat</title>
         <meta
           name="description"
-          content="Explore Cob's Farm, a unique adventure game that combines farming and combat. Join the Alpha Fields and start your adventure today."
+          content="Explore Cob&apos;s Farm, a unique adventure game that combines farming and combat. Join the Alpha Fields and start your adventure today."
         />
-        <meta name="keywords" content="Cob's Farm, adventure game, farming, combat, Alpha Fields" />
+        <meta name="keywords" content="Cob&apos;s Farm, adventure game, farming, combat, Alpha Fields" />
         <link rel="icon" href="/TwitterPfp.png" />
       </Head>
       <Hero />
