b32d6b603b3d1c875707c64f20e04eff80cb4791
cobmin <jacob-huber@outlook.com>
2023-09-22 23:34:05 -0500

    image edit for small screens last chance before ai

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,23 +3,19 @@ import { DiscordLogo, LooperLandsLogo, TwitterLogo } from "./image";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-white p-6 md:p-12">
+        <footer className="bg-gray-800 text-white p-6 md:p-6">
             <div className="flex flex-col md:flex-row justify-between items-center">
 
-                <div className="mb-4 md:mb-0 md:text-left sm:text-center">
+                <div className=" md:mb-0 md:text-left sm:text-center">
                     <h2 className="text-lg font-semibold">Stay Connected</h2>
-                    <ul className="flex space-x-4">
-                        <li>
-                            <a href="https://twitter.com/CobsFarm" target="_blank" rel="noreferrer">
-                                <TwitterLogo />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://discord.gg/BAGXJZVH4Y" target="_blank" rel="noreferrer">
-                                <DiscordLogo />
-                            </a>
-                        </li>
-                    </ul>
+                    <div className='columns-2'>
+                        <a href="https://twitter.com/CobsFarm" target="_blank" rel="noreferrer">
+                            <TwitterLogo />
+                        </a>
+                        <a href="https://discord.gg/BAGXJZVH4Y" target="_blank" rel="noreferrer">
+                            <DiscordLogo />
+                        </a>
+                    </div>
                 </div>
 
                 <div className="flex justify-center md:justify-end">
--- components/image.tsx
+++ components/image.tsx
@@ -9,10 +9,9 @@ import DiscordLogos from '../public/discord.svg'
 
 export function FarmBackground() {
     return (
-        <Image src={Farm} alt="Picture of the author" layout='fill' objectFit='cover' />
+        <Image src={Farm} alt="Cob's Farm" layout='fill' objectFit='cover' />
     )
 }
-
 export function CobsFarmLogo() {
     return (
         <Image src={CobsLogo} alt="Cob's Farm Logo" />
@@ -25,14 +24,11 @@ export function CobsFarmLogoNav() {
         />
     )
 }
-
-
 export function CobsEntrance() {
     return (
-        <Image src={Entrance} objectFit="contain" className="h-full w-auto rounded" alt="Cob's Farm Entrance" />
+        <Image src={Entrance} className="h-full w-auto rounded aspect-auto" alt="Cob's Farm Entrance" />
     )
 }
-
 export function LooperLandsLogo() {
     return (
         <Image src={LooperLands} className='rounded w-32 h-32' alt="Looper Lands Logo" />
@@ -40,17 +36,17 @@ export function LooperLandsLogo() {
 }
 export function DisplayAlphaLoopers() {
     return (
-        <Image src={DisplayLoopers} objectFit="contain" className="h-full w-auto rounded" alt="Looper Lands Logo" />
+        <Image src={DisplayLoopers} className="h-full w-auto rounded aspect-auto" alt="Alpha Loopers" />
     )
 }
 
 export function TwitterLogo() {
     return (
-        <Image src={TwitterLogos} objectFit="contain" className=" w-12 h-12" alt="Looper Lands Logo" />
+        <Image src={TwitterLogos} className="w-12 h-12" alt="Twitter Logo" />
     )
 }
 export function DiscordLogo() {
     return (
-        <Image src={DiscordLogos} objectFit="contain" className=" w-12 h-12" alt="Looper Lands Logo" />
+        <Image src={DiscordLogos} className=" w-12 h-12 " alt="Discord Logo" />
     )
 }
